ca85b1f4fbdc5c08ce174b753329c1981528a84e
Chris Mason <cmason@olson.com>
2013-12-25 05:35:54 -0600

    Adding blank config dist

---
+++ server/config.dist.js
@@ -0,0 +1,34 @@
+(function(){
+
+    module.exports = {
+        debug: true,
+        logLevel: 'spark',
+        log: function(who){
+            return this.logLevel.indexOf(who) > -1;
+        },
+        tcp: {
+            port: 5000
+        },
+        logger: {
+            firebase: {
+                id: 'spark-logger',
+                logs: '/logs/'
+            }
+        },
+        spark: {
+            token: '<token>',
+            cores: [
+                '<coreid>'
+            ]
+        },
+        spotimote: {
+            server: '<ipaddress>',
+            debug: false
+        }
+    };
+
+}).call(this);
+
+
+
+
