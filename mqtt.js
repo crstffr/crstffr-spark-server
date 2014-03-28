49aaab22dd894f786d32e7f31bae4553426119c3
Christopher Mason <crstffr@gmail.com>
2014-03-28 07:13:06 +0000

    MQTT commands to configure device upon connect

--- mqtt.js
+++ mqtt.js
@@ -1,17 +1,55 @@
 (function(undefined) {
 
     var ip = require('ip');
+    var mqtt = require('mqtt');
     var util = require('./server/util');
     var config = require('./server/config');
-
-    var mqtt = require('mqtt');
+    var prefix = 'dev/48ff6b065067555039091087/';
 
     client = mqtt.createClient(1883, 'localhost');
 
     client.subscribe('#');
 
     client.on('message', function(topic, message, packet) {
-        console.log(topic, message);
+
+        console.log(topic.substr(29), message);
+
+        if (topic == prefix + 'action/configure') {
+            if (message == 'true') {
+                client.publish(prefix + 'setup/debug', 'false');
+                client.publish(prefix + 'setup/default/radio/station', '879');
+                client.publish(prefix + 'control/radio/station', '879');
+                client.publish(prefix + 'control/radio/volume', '15');
+                client.publish(prefix + 'setup/led/max/intensity', '100');
+                client.publish(prefix + 'control/led/intensity', '2');
+                client.publish(prefix + 'control/led/color', 'orange');
+            }
+        }
+
+        if (topic == prefix + 'action/connected') {
+            if (message == 'true') {
+                client.publish(prefix + 'control/power', 'on');
+            }
+        }
+
+        if (topic == prefix + 'action/button/enc') {
+            if (message == 'press') {
+                client.publish(prefix + 'control/power', 'toggle');
+            }
+        }
+
+        if (topic == prefix + 'action/button/led') {
+            if (message == 'press') {
+                client.publish(prefix + 'control/led/dim', '1');
+            }
+        }
+
+        if (topic == prefix + 'action/button/misc') {
+            if (message == 'press') {
+                client.publish(prefix + 'control/radio', 'skip');
+            }
+        }
+
     });
 
     console.log("Server on: " + ip.address());
@@ -21,9 +59,14 @@
     process.stdin.on('data', function(chunk) {
 
         var chunk = chunk.trim();
-        var parts = chunk.split(" ");
+        var parts = chunk.split(' ');
+        var topics = parts[0].split('/');
 
-        client.publish('dev/48ff6b065067555039091087/control/' + parts[0], parts[1], {qos: 1});
+        if (topics[0] == 'setup') {
+            client.publish(prefix + parts[0], parts[1], {qos: 1});
+        } else {
+            client.publish(prefix + 'control/' + parts[0], parts[1], {qos: 1});
+        }
 
         // power on
         // power off
